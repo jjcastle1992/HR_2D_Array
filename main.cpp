54cafba8df6654905329fa27b9f2c129703116a2
James <jjcastle@utexas.edu>
2022-06-28 22:00:19 -0500

    hourglassSum: revised bug with for loops where it was stopping 1 row and column to soon.

--- main.cpp
+++ main.cpp
@@ -23,8 +23,8 @@ int hourglassSum(vector<vector<int>> arr) {
     int maxSum = -9999;
     int sum = 0;
     //Build 2d for loop
-    for (int row = 0; row < 3; row++) {
-        for (int col = 0; col < 3; col++) {
+    for (int row = 0; row < 4; row++) {
+        for (int col = 0; col < 4; col++) {
             sum = (arr[row][col] + arr[row][col + 1] + arr [row][col + 2]) +
                       (arr [row + 1][col + 1]) +
                       (arr[row + 2][col] + arr[row + 2][col + 1] + arr [row + 2][col + 2]);
